2c84159306925ec0238a40da66a170ca80551475
iki1uc <entertrainer@web.de>
2026-07-26 22:56:40 +0200

    Update e.signal.js

--- E/e.signal.js
+++ E/e.signal.js
@@ -1,14 +1,12 @@
 export const ESignal = {
   color(errors) {
-    if (errors.length === 0) return "green";
+    if (!errors || errors.length === 0) return "green";
     if (errors.length === 1) return "yellow";
     return "red";
   },
 
   apply(element, color) {
-    element.style.color =
-      color === "green" ? "#0f0" :
-      color === "yellow" ? "#ff0" :
-      "#f00";
+    element.classList.remove("green", "yellow", "red");
+    element.classList.add(color);
   }
 };
