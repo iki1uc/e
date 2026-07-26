339716181a432320495c7064fd12e1c3670d1a02
iki1uc <entertrainer@web.de>
2026-07-26 22:56:28 +0200

    Update e.scan.js

--- E/e.scan.js
+++ E/e.scan.js
@@ -1,7 +1,10 @@
 export const EScan = {
   async read(path) {
-    const res = await fetch(path);
-    const data = await res.text();
-    return data.split("\n");
+    try {
+      const res = await fetch(path);
+      return await res.text();
+    } catch (err) {
+      return "fail";
+    }
   }
 };
